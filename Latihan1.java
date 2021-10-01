a664769483c95d402bc20b27f39ae7d3427b4e6d
sadamaction <81281142+sadamaction@users.noreply.github.com>
2021-10-01 09:30:49 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author Sadam Fadhilah
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        int uang = 50000;
+        int harga = 55000;
+        
+        String jawab = uang < harga ? "ya":"Tidak";
+        
+        System.out.println(jawab);
+    }
+    
+}
---
+++ Latihan2.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author Sadam Fadhilah
+ */
+public class Latihan2 {
+
+    public static void main(String[] args) {
+        int a = 100 & 20;
+        int b = 528 | 70;
+        int c = 256 >> 3;
+        int d = 128 << 5;
+        int e = 600 >> 4;
+        int f = 423 << 3;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+    
+}
